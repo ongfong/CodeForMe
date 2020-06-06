e094fdbf6dce8823235b0b2ba48ec4909609eb84
hipeople <dongfong9@gmail.com>
2020-06-06 13:24:13 +0700

    Update about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,39 +1,75 @@
 import React from 'react';
 
 import classes from './About.css';
+import Image from '../../assets/profile3.jpg';
 
 const about = () => {
   return (
     <div className={classes.container}>
-      <h1>About Enfant K.</h1>
-      <div className={classes.content}>
-        <div className={classes.content_profile}>
-          <h2>Puangporn Kowsamruang</h2>
+      <div className={classes.aboutme_left}>
+        <h1>About enfant K.</h1>
+        <h2>I'm a FrontEnd Developer from Bangkok, Thailand</h2>
+        <div className={classes.aboutme_left_content}>
+          <p>
+            I enjoy developing and design the web application whether a be HTML,
+            CSS, Reactjs Redux and{' '}
+            <strong>
+              I love The Nature. <i class="fab fa-pagelines fa-2x"></i>
+            </strong>
+          </p>
+        </div>
+        <div className={classes.aboutme_left_content}>
           <h3>Education</h3>
           <p>Bachelor of Science (Computer Science), Thammasat University</p>
-          <p>(November 2016 — June 2020)</p>
+          <p>(November 2016 — June 2020).</p>
         </div>
-        <div className={classes.content_contact}>
-          <h3>Front-end Developer.</h3>
-          <p>
-            An experienced web Developer by building using MERN Stack with
-            React/Next.js.
-          </p>
-          <h3>Contact</h3>
-          <div className={classes.content_contact_icon}>
-            <span style={{color: 'Dodgerblue'}}>
-              <i class="fas fa-envelope fa-2x"></i>
-              <p>Eh_2939@hotmail.com</p>
-            </span>
-            <span style={{color: 'Mediumslateblue'}}>
-              <i class="fab fa-github fa-2x"></i>
-              <p>https://github.com/ongfong/</p>
-            </span>
-          </div>
+        <div className={classes.aboutme_left_content}>
+          <h3>Interships</h3>
+          <p>Frontend Developer, Derive innovation Company Limited</p>
+          <p>(June 2019 — August 2019).</p>
         </div>
+        {/* <div className={classes.aboutme_left_icon}>
+          <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+          <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+        </div> */}
+      </div>
+      <div className={classes.aboutme_right}>
+        <img src={Image} />
       </div>
     </div>
   );
 };
 
 export default about;
+
+{
+  /* <div className={classes.container}>
+<h1>About Enfant K.</h1>
+<div className={classes.content}>
+  <div className={classes.content_profile}>
+    <h2>Puangporn Kowsamruang</h2>
+    <h3>Education</h3>
+    <p>Bachelor of Science (Computer Science), Thammasat University</p>
+    <p>(November 2016 — June 2020)</p>
+  </div>
+  <div className={classes.content_contact}>
+    <h3>Front-end Developer.</h3>
+    <p>
+      An experienced web Developer by building using MERN Stack with
+      React/Next.js.
+    </p>
+    <h3>Contact</h3>
+    <div className={classes.content_contact_icon}>
+      <span style={{color: 'Dodgerblue'}}>
+        <i class="fas fa-envelope fa-2x"></i>
+        <p>Eh_2939@hotmail.com</p>
+      </span>
+      <span style={{color: 'Mediumslateblue'}}>
+        <i class="fab fa-github fa-2x"></i>
+        <p>https://github.com/ongfong/</p>
+      </span>
+    </div>
+  </div>
+</div>
+</div> */
+}
--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -3,13 +3,9 @@ import {Route, Link} from 'react-router-dom';
 
 import {makeStyles} from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Typography from '@material-ui/core/Typography';
 
 import classes from './Toolbar.css';
-import Image from '../../../assets/image2.jpg';
+import Image from '../../../assets/showcase.jpg';
 
 import About from '../../About/About';
 import Projects from '../../Projects/Projects';
@@ -35,44 +31,28 @@ const toolbar = () => {
   const useStyle = useStyles();
 
   return (
-    <React.Fragment>
-      <div className={classes.container}>
-        <Toolbar className={useStyle.toolbar}>
-          <Button size="small">Subscribe</Button>
-          <Typography
-            component="h2"
-            variant="h5"
-            color="inherit"
-            align="center"
-            noWrap
-            className={useStyle.toolbarTitle}
-          ></Typography>
-          <IconButton>
-            <SearchIcon />
-          </IconButton>
-          <Button variant="outlined" size="small">
-            Sign up
-          </Button>
-        </Toolbar>
-        <Toolbar
-          component="nav"
-          variant="dense"
-          className={classes.toolbarSecondary}
-        >
-          <Link to="/projects" className={classes.item}>
-            Projects
-          </Link>
-          <Link to="/about" className={classes.item}>
-            About
-          </Link>
-        </Toolbar>
-        <img src={Image} className={classes.showcase} />
-        <div>
-          <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects} />
-        </div>
+    <div className={classes.container}>
+      <Toolbar className={useStyle.toolbar}></Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        className={classes.toolbarSecondary}
+      >
+        <Link to="/projects" className={classes.item}>
+          Projects
+        </Link>
+        <Link to="/about" className={classes.item}>
+          About
+        </Link>
+      </Toolbar>
+      <div className={classes.showcase}>
+        <img src={Image} />
       </div>
-    </React.Fragment>
+      <div>
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+      </div>
+    </div>
   );
 };
 
